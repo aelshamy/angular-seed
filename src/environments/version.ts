01fce2964991bf65ca5c29c7a1dfdcbc3e177b3b
Elshamy, Amr <elshaa1@nationwide.com>
2018-03-06 15:28:02 -0500

    add version support

---
+++ src/environments/version.ts
@@ -0,0 +1,14 @@
+// IMPORTANT: THIS FILE IS AUTO GENERATED! DO NOT MANUALLY EDIT OR CHECKIN!
+/* tslint:disable */
+export const VERSION = {
+	dirty: true,
+	raw: "7cc0655-dirty",
+	hash: "7cc0655",
+	distance: null,
+	tag: null,
+	semver: null,
+	suffix: "7cc0655-dirty",
+	semverString: null,
+	version: "0.0.0"
+};
+/* tslint:enable */
