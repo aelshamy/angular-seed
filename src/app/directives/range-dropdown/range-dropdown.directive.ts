61dffd0b676f73d1f3db52d464efce79dd1e3331
Amr Mohamed <ammohamed@icct.com>
2018-04-02 01:12:34 -0400

    user destructure and set type defintion

--- src/app/directives/range-dropdown/range-dropdown.directive.ts
+++ src/app/directives/range-dropdown/range-dropdown.directive.ts
@@ -12,16 +12,16 @@ export class RangeDropdownDirective {
 	) {}
 
 	@Input()
-	set appRangeDropdown(value) {
+	set appRangeDropdown([from, to]: [number, number]) {
 		this.viewContainerRef.clear();
-		this.range = this.generateRange(value[0], value[1]);
+		this.range = this.generateRange(from, to);
 		this.range.map(number =>
 			this.viewContainerRef.createEmbeddedView(this.templateRef, {
 				$implicit: number
 			})
 		);
 	}
-	private generateRange(from, to): number[] {
+	private generateRange(from: number, to: number): number[] {
 		return new Array(to - from + 1).fill(0).map((_, index) => from + index);
 	}
 }
