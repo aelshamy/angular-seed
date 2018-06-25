f96253f22d66a765f1efad244870ab7add51dbb4
Amr Mohamed <ammohamed@icct.com>
2018-06-24 21:38:46 -0400

    add test coverage script

--- src/app/directives/range-dropdown/range-dropdown.directive.spec.ts
+++ src/app/directives/range-dropdown/range-dropdown.directive.spec.ts
@@ -2,7 +2,7 @@ import { RangeDropdownDirective } from "./range-dropdown.directive";
 
 describe("RangeDropdownDirective", () => {
 	it("should create an instance", () => {
-		const directive = new RangeDropdownDirective();
+		const directive = new RangeDropdownDirective(null, null);
 		expect(directive).toBeTruthy();
 	});
 });
